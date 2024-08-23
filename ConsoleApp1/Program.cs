d0589b46bdaf7f4e2463004f309562a905559c85
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-08-23 12:51:59 +0530

    2nd update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+namespace ConsoleApp1
+{
+    class Program{
+        static void Main(string[] args){
+            Addbook();
+        }
+
+        public static void Addbook(){
+            Library l1=new Library();
+            Console.WriteLine("Enter book Nmae");
+            string Bname=Console.ReadLine();
+            l1.Bookname=Bname;
+        }
+    }
+}
---
+++ ConsoleApp1/library.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+namespace ConsoleApp1{
+    class Library{
+        public string Bookname{get;set;}
+
+        public void Addbook(){
+            
+        }
+    }
+}
